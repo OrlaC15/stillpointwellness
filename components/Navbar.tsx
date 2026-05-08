9b3fecef15b0629cf9fdbe61decbbf41c0b4485f
OrlaC15 <16900339+OrlaC15@users.noreply.github.com>
2026-05-08 20:06:44 +0100

    update 10

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex justify-between p-6 border-b">
-      <div className="font-bold">Dr Lisa Style Site</div>
+      <div className="font-bold">Stillpoint Wellness - Orla Cullen</div>
       <div className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/therapy">Therapy</Link>
-        <Link href="/events">Events</Link>
         <Link href="/about">About</Link>
       </div>
     </nav>
