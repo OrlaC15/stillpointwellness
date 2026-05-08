0a0acd524ddf58d5e52c478d1f320ebefd020902
OrlaC15 <16900339+OrlaC15@users.noreply.github.com>
2026-05-08 20:03:04 +0100

    update 9

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 
 const headingFont = Cormorant_Garamond({
